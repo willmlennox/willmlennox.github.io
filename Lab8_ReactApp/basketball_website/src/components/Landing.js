01345fa6bd8c1700cefe85214c3d65756769e916
Colin McFarlane <colinmcf0@gmail.com>
2022-03-22 23:06:21 -0400

    Update landing page

--- Lab8_ReactApp/basketball_website/src/components/Landing.js
+++ Lab8_ReactApp/basketball_website/src/components/Landing.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import { Carousel } from "react-bootstrap";
 import { Button } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import { CardGroup } from "react-bootstrap";
 
 // import img1 from "../images/basketball1.jpg";
 // import img2 from "../images/basketball2.jpg";
@@ -14,7 +16,7 @@ const Landing = () => {
     const img3 = require("../images/basketball3.jpg")
 
     return (
-        <Container fluid className="mt-3 bg-light p-5">
+        <Container fluid className="bg-light p-5">
             <Carousel fade>
                 <Carousel.Item>
                     <img
@@ -58,13 +60,48 @@ const Landing = () => {
                 </Carousel.Item>
             </Carousel>
 
-        <h2>Welcome to the Somerset Basketball League</h2>
-
-        <Button>view services
-        </Button>
-
+            <Container className="text-center p-3">
+                <h1 className="mt-5">Our Services</h1>
+                <CardGroup className="p-5">
+                    <Card border="secondary">
+                        <Card.Img variant="top" src="" />
+                        <Card.Body>
+                        <Card.Title>Basketball Court Rental</Card.Title>
+                        <Card.Text className="p-3">
+                            We have a variety of indoor and outdoor courts available for booking all over Ottawa.
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <Button variant="warning">Book a Court</Button>
+                        </Card.Footer>
+                    </Card>
+                    <Card border="secondary">
+                        <Card.Img variant="top" src="" />
+                        <Card.Body>
+                        <Card.Title>Teams and Leagues</Card.Title>
+                        <Card.Text className="p-3">
+                            Our leagues are active year round. Find and join a team at any skill level and age group.
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <Button variant="warning">Join a Team</Button>
+                        </Card.Footer>
+                    </Card>
+                    <Card border="secondary">
+                        <Card.Img variant="top" src="" />
+                        <Card.Body>
+                        <Card.Title>Training and Coaching</Card.Title>
+                        <Card.Text className="p-3">
+                            Train and improve your basketball skills. Choose from a variety of coaches, trainers, and classes.
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <Button variant="warning">Find a Trainer</Button>
+                        </Card.Footer>
+                    </Card>
+                    </CardGroup>
+            </Container>
          </Container>
-
     )
 }
 
