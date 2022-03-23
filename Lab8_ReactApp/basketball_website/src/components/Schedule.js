71cee542b9c440c4a571766afe9ac7347e48a3b4
ryanmccarron <38532048+ryanmccarron@users.noreply.github.com>
2022-03-23 00:53:11 -0400

    Schedule page with working calendar added

--- Lab8_ReactApp/basketball_website/src/components/Schedule.js
+++ Lab8_ReactApp/basketball_website/src/components/Schedule.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Calendar from 'react-calendar';
 import { Container } from "react-bootstrap";
 
 const Schedule = () => {
@@ -6,11 +7,13 @@ const Schedule = () => {
     return (
         <Container fluid className="mt-3 bg-light p-5">
             
-            <div>
-                Schedule
+            <div class="col-md-12">
+                <h2>Schedule</h2> 
+                <p> Displayed below is the curent schedule, as bookings are created the calendar will be updated.</p>
+                <Calendar />
             </div>
 
-         </Container>
+        </Container>
 
     )
 }
