7b94da0b00bdc49fcf5d39da70825eb395909663
ryanmccarron <38532048+ryanmccarron@users.noreply.github.com>
2022-03-29 20:30:18 -0400

    larger calendar with a couple days selected

--- Lab9_ReactApp/basketball_website/src/components/Schedule.js
+++ Lab9_ReactApp/basketball_website/src/components/Schedule.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import Calendar from 'react-calendar';
 
 const Schedule = () => {
 
     return (
         <Container fluid className="mt-3 bg-light p-5">
             
-            <div>
-                Schedule
+            <div class="col-md-12">
+                <h2>Schedule</h2> 
+                <p> Displayed below is the curent schedule, as bookings are created the calendar will be updated.</p>
+                <Calendar
+                class = "calendar-cust" 
+                value={[new Date(2022, 3, 29), new Date(2022, 3, 28)]}
+                />
             </div>
 
-         </Container>
+        </Container>
 
     )
 }
